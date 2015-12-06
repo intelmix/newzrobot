8fb2ed86858ab36758a97a6164a112eb48c80c28
Mahdi Mohammadi <mm@binary.com>
2015-12-06 17:45:08 -0500

    support to return multiple elements as a response of REST API

--- server/src/main/java/newzrobot/MainController.java
+++ server/src/main/java/newzrobot/MainController.java
@@ -6,17 +6,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
 
 @RestController
 public class MainController {
     private static final String template = "HEllo %s";
+    private static final String template2 = "another hello %s";
     private final AtomicLong counter = new AtomicLong();
     
     @RequestMapping("/news")
-    public NewsItem news(@RequestParam(value="name", defaultValue="world") String name ) {
+    public ArrayList<NewsItem> news(@RequestParam(value="name", defaultValue="world") String name ) {
         System.out.println("Configured for "+name);
+        
+        ArrayList<NewsItem> result = new ArrayList<NewsItem>();
 
-        return new NewsItem(counter.incrementAndGet(), String.format(template, name));
+        result.add(new NewsItem(counter.incrementAndGet(), String.format(template, name)));
+        result.add(new NewsItem(counter.incrementAndGet(), String.format(template2, name)));
+
+        return result;
     }
 
 }
