374c18a1547933422cefc15c5ed687db013f00f9
Mahdi Mohammadi <mm@binary.com>
2015-12-19 18:11:41 -0500

    reading from a statis feed and writing to MongoDB

--- crawler/src/main/java/com/newzrobot/crawler/Application.java
+++ crawler/src/main/java/com/newzrobot/crawler/Application.java
@@ -5,67 +5,98 @@ import com.rometools.rome.io.*;
 import com.rometools.rome.feed.synd.*;
 import java.net.URL;
 import java.util.List;
+import java.util.Date;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
+import java.security.MessageDigest;
 
 
 public class Application {
-    public static void main(String[] args) throws java.net.MalformedURLException,java.io.IOException, com.rometools.rome.io.FeedException  {
 
+    //TODO: better logging for info, warn and exceptions
+    //TODO: use LogEntries if it does not need much overhead
+    //TODO: Move all moving parts to a config filec
+    public static void main(String[] args) throws java.net.UnknownHostException,java.net.MalformedURLException,java.io.IOException, com.rometools.rome.io.FeedException  {
+
+        /*                          List<String> dbs = mongo.getDatabaseNames();
+                  for(String db : dbs){
+                  System.out.println("Database: " + db);
+
+                  }
+
+                  DB db = mongo.getDB("data"); 
+                  DBCollection table = db.getCollection("user");
+
+                  BasicDBObject document = new BasicDBObject();
+                  document.put("name", "mkyong");
+                  document.put("age", 30);
+                  table.insert(document);
+
+                  return;
+                  */
         MongoClient mongo = new MongoClient( "localhost" , 27017 );
-        List<String> dbs = mongo.getDatabaseNames();
-        for(String db : dbs){
-            System.out.println("Database: " + db);
-
-        }
-
         DB db = mongo.getDB("data"); 
-        DBCollection table = db.getCollection("user");
+        DBCollection feed_entry = db.getCollection("feed_entry");
 
-        BasicDBObject document = new BasicDBObject();
-        document.put("name", "mkyong");
-        document.put("age", 30);
-        table.insert(document);
+        //URL feedUrl = new URL("https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&output=rss");
+        URL feedUrl = new URL("http://www.latimes.com/rss2.0.xml");
 
-        return;
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = input.build(new XmlReader(feedUrl));
 
-
-        /*        URL feedUrl = new URL("https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&output=rss");
-
-                  SyndFeedInput input = new SyndFeedInput();
-                  SyndFeed feed = input.build(new XmlReader(feedUrl));
-
-                  System.out.println("Feed Title: " + feed.getTitle());
-                  int counter=0;
+        System.out.println("Feed Title: " + feed.getTitle());
+        int counter=0;
 
         // Get the entry items...
         for (SyndEntry entry : (List<SyndEntry>) feed.getEntries()) {
-        counter++;
-        if ( counter > 5) break;
-        System.out.println("Title: " + entry.getTitle());
-        System.out.println("Unique Identifier: " + entry.getUri());
-        System.out.println("Updated Date: " + entry.getUpdatedDate());
+            counter++;
+            if ( counter > 3) break;
 
-        // Get the Links
-        for (SyndLink link : (List<SyndLink>) entry.getLinks()) {
-        System.out.println("Link: " + link.getHref());
-        }            
+            String title = entry.getTitle();
+            String uri = entry.getUri();
+            Date date = entry.getPublishedDate();
+            long epoch = date.getTime();
+            String _id = md5(title + String.valueOf(epoch) + uri);
 
-        // Get the Contents
-        for (SyndContent content : (List<SyndContent>) entry.getContents()) {
-        System.out.println("Content: " + content.getValue());
+            BasicDBObject document = new BasicDBObject();
+            document.put("_id", _id);
+            document.put("t", title);
+            document.put("u", uri);
+            document.put("e", epoch);
+            feed_entry.insert(document);
+
+            System.out.println("GUID: " + _id);
+            System.out.println("Title: " + title);
+            System.out.println("Unique Identifier: " + uri);
+            System.out.println("Published Date: " + epoch);
+
+
+            System.out.println("");
         }
 
-        // Get the Categories
-        for (SyndCategory category : (List<SyndCategory>) entry.getCategories()) {
-        System.out.println("Category: " + category.getName());
-        }
-        }
+        System.out.println("Done!");
+    }
 
-        System.out.println("Done!");*/
+    public static String md5(String base) {
+        try{
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
     }
 }
