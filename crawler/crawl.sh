66527b6a5e5d878681e8233f61beed1408d31ffb
Mahdi Mohammadi <mm@binary.com>
2016-01-04 17:44:25 -0500

    setting up cron for reading feeds

---
+++ crawler/crawl.sh
@@ -0,0 +1,2 @@
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> /srv/newzrobot/crawler/crawl.log
+/usr/bin/java -jar /srv/newzrobot/crawler/target/newzrobot-crawler-0.1.0.jar >> /srv/newzrobot/crawler/crawl.log
