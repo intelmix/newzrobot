435b3f5986931b908f8fcd52bb006d836c355133
Mahdi Mohammadi <mm@binary.com>
2016-01-05 17:43:30 -0500

    adding more rss feeds

--- crawler/crawl.sh
+++ crawler/crawl.sh
@@ -1,2 +1,2 @@
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> /srv/newzrobot/crawler/crawl.log
-/usr/bin/java -jar /srv/newzrobot/crawler/target/newzrobot-crawler-0.1.0.jar >> /srv/newzrobot/crawler/crawl.log
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> /tmp/crawl.log
+/usr/bin/java -jar /srv/newzrobot/crawler/target/newzrobot-crawler-0.1.0.jar >> /tmp/crawl.log
